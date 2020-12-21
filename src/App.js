b8d286ac516c3efd45dd161009f8215d2b3de330
Saim Raza Gondal <sgondal.bscs16seecs@seecs.edu.pk>
2020-12-22 00:28:10 +0500

    send message and receive message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import io from "socket.io-client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const socket = useRef();
 
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     socket.current = io("localhost:8080");
+    socket.current.on("message", (text) => {
+      console.log(text);
+      setMessages((prev) => [...prev, text]);
+    });
   }, []);
 
-  const dummy = () => {
-    console.log("hi");
-    socket.emit("connection");
+  const onSend = () => {
+    socket.current.emit("message", text);
+    setText("");
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p onClick={dummy}>Click</p>
+        <input
+          type="text"
+          placeholder="Enter text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyPress={(e) => e.key === "Enter" && onSend()}
+        />
+        {messages.map((el, index) => (
+          <p key={index}>{el}</p>
+        ))}
       </header>
     </div>
   );
